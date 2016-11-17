f97239a2f13569e0d31114990564464994167630
Ivan Sovic <ivan.sovic@gmail.com>
2016-11-17 17:54:27 +0100

    Fixed bugs in aracon and example1-lambda.sh scripts, and changed the commit version of racon in the Makefile.

--- example1-lambda.sh
+++ example1-lambda.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 mkdir -p results/example1-lambda/dnadiff/
-memtime="results/example1-lambda-erc/total.memtime"
+memtime="results/example1-lambda/total.memtime"
 /usr/bin/time --format "Command line: %C\nReal time: %e s\nCPU time: -1.0 s\nUser time: %U s\nSystem time: %S s\nMaximum RSS: %M kB\nExit status: %x" --quiet -o ${memtime} \
 	./aracon test-data/lambda/reads.fastq results/example1-lambda/ --num-threads 4
 
